96ebfca54c8c7f1be4988b8286bce1076c8a4fde
avyukd <avyukd@gmail.com>
2017-08-25 12:14:07 -0400

    Add files via upload
    All unique code files, everything else auto generated by django. Code before google maps API added and before search bar correctly added. Some bug fixes on forms needed

--- urls.py
+++ urls.py
@@ -1,18 +1,22 @@
-from django.conf.urls import url
+"""linkpediav1 URL Configuration
 
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
 
 urlpatterns = [
-    url(r'^login/', views.login, name='login'),
-	url(r'^home/expert/(?P<username>[\w\-]+)$', views.home, name='home'),
-	url(r'^home/user/(?P<username>[\w\-]+)$', views.home, name='home'),
-	url(r'^viewprofile/(?P<username>[\w\-]+)', views.viewprofile, name='viewprofile'),
-	url(r'^addcontent/(?P<username>[\w\-]+)$', views.addcontent, name='addcontent'),
-	url(r'^addlink/(?P<username>[\w\-]+)$', views.addlink, name='addlink'),
-	url(r'^(?P<id>[0-9]+)$', views.linkinfo, name='linkinfo'),
-	url(r'^signupint/', views.signupint, name='signupint'),
-	url(r'^signupform/', views.signupform, name='signupform'),
-	url(r'^signupexpert/', views.signupexpert, name='signupexpert'),
-	url(r'^contactus/', views.contactus, name='contactus'),
-	
+    url(r'^linkpedia/', include('linkpedia.urls')),
+    url(r'^admin/', admin.site.urls),
 ]
